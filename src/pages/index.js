69fd1cda15e4d181b6a295f738f8cca1cef6f6a3
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2023-02-25 00:14:58 +0800

    nc

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,5 @@ export { default as Login } from "./Login";
 export { default as Support } from "./Support";
 export { default as Summary } from "./Summary";
 export { default as Register } from "./Register";
+export { default as Dashboard } from "./Dashboard";
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,7 @@ export default defineConfig({
   define: {
     global: "window",
   },
-})
+  build: {
+    chunkSizeWarningLimit: 500000, // 500KB
+  },
+});
