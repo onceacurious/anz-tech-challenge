94880f9ba88c1aa0e931510fbeb886a8f2a8a183
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2023-02-28 15:21:50 +0800

    nc

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -140,7 +140,8 @@ const Dashboard = () => {
               {divisions.map((d, x) => (
                 <Button key={x} onClick={() => handleChangeTopic(d.divisionId)}>
                   {`${d.title} ${
-                    topic.filter((t) => t.divisionId == d.divisionId).length
+                    data.topic.filter((t) => t.divisionId == d.divisionId)
+                      .length
                   }`}
                 </Button>
               ))}
